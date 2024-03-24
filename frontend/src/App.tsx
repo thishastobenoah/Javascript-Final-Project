b578ace97e1399c9e97f86b08c8e47cf4c10d1ce
thishastobenoah <thishastobenoah@gmail.com>
2024-03-24 13:00:37 -0400

    adding app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,33 @@
-import { useState } from 'react'
-import './App.css'
-import { Routes, Route, NavLink } from 'react-router-dom';
+import React, { useState } from 'react';
+import Heroes from './Heroes';
+import Valorant from './Valorant';
+import League from './League';
 
+const App = () => {
+  const [selectedGame, setSelectedGame] = useState('');
 
-function App() {
+  const renderGameComponent = () => {
+    switch (selectedGame) {
+      case 'heroes':
+        return <Heroes />;
+      case 'valorant':
+        return <Valorant />;
+      case 'league':
+        return <League />;
+      default:
+        return <div>Please select a game</div>;
+    }
+  };
 
   return (
-    <>
-      
-    </>
-  )
-}
+    <div>
+      <h1>Game Stats Tracker</h1>
+      <button onClick={() => setSelectedGame('heroes')}>Heroes</button>
+      <button onClick={() => setSelectedGame('valorant')}>Valorant</button>
+      <button onClick={() => setSelectedGame('league')}>League of Legends</button>
+      {renderGameComponent()}
+    </div>
+  );
+};
 
-export default App
+export default App;
